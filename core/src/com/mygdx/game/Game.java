f3f823a3f8797a5b959dab8d3d2b925c127a8ffa
gfor-gour <talukdargour.bd@gmail.com>
2023-08-06 17:07:06 +0600

    9th Commit
    added some background music

--- core/src/com/mygdx/game/Game.java
+++ core/src/com/mygdx/game/Game.java
@@ -2,6 +2,7 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.mygdx.game.states.GameStateManager;
@@ -14,12 +15,16 @@ public class Game extends ApplicationAdapter {
 	public static final String TITLE = "Fly And Live";
 	private GameStateManager gsm;
 	private SpriteBatch batch;
-
+	private Music music;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		gsm = new GameStateManager();
+		music = Gdx.audio.newMusic(Gdx.files.internal("music.mp3"));
+		music.setLooping(true);
+		music.setVolume(0.3f);
+		music.play();
 		Gdx.gl.glClearColor(1,0,0,1);
 		gsm.push(new MenuState(gsm));
 	}
@@ -31,6 +36,12 @@ public class Game extends ApplicationAdapter {
 		gsm.update(Gdx.graphics.getDeltaTime());
 		gsm.render(batch);
 
+	}
+
+	@Override
+	public void dispose() {
+		super.dispose();
+		music.dispose();
 	}
 
 }
