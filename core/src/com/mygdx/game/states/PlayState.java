c37bf3a3b83c03219a54777b84897f83f13c0663
gfor-gour <talukdargour.bd@gmail.com>
2023-08-06 15:10:14 +0600

    7th commit

--- core/src/com/mygdx/game/states/PlayState.java
+++ core/src/com/mygdx/game/states/PlayState.java
@@ -3,20 +3,30 @@ package com.mygdx.game.states;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.Array;
 import com.mygdx.game.Game;
 import com.mygdx.game.Sprites.Plane;
 import com.mygdx.game.Sprites.Tube;
 
 public class PlayState extends State  {
+
+    private static final int TUBE_SPACING = 125;
+    private static final int TUBE_COUNT = 4;
     private Plane plane;
     private Texture bg;
-    private Tube tube;
+
+    private com.badlogic.gdx.utils.Array<Tube> tubes;
     public PlayState(GameStateManager gsm) {
         super(gsm);
         plane = new Plane(50,300);
         cam.setToOrtho(false, Game.WIDTH/2,Game.HEIGHT/2);
         bg = new Texture("bg.png");
-        tube = new Tube(100);
+
+        tubes = new Array<Tube>();
+            for(int i=1; i<= TUBE_COUNT; i++){
+                tubes.add(new Tube(i*(TUBE_SPACING + Tube.TUBE_WIDTH)));
+            }
+
     }
 
     @Override
@@ -29,6 +39,14 @@ public class PlayState extends State  {
     public void update(float dt) {
         handleInput();
         plane.update(dt);
+        cam.position.x = plane.getPosition().x + 80;
+        for(Tube tube: tubes){
+            if(cam.position.x - (cam.viewportWidth/2)> tube.getPosTopTube().x + tube.getTopTube().getWidth()){
+                tube.reposition(tube.getPosTopTube().x + ((Tube.TUBE_WIDTH + TUBE_SPACING) * TUBE_COUNT));
+            }
+        }
+        cam.update();
+
     }
 
     @Override
@@ -37,8 +55,10 @@ public class PlayState extends State  {
         sb.begin();
         sb.draw(bg, cam.position.x - (cam.viewportWidth/2),0);
         sb.draw(plane.getPlane(),plane.getPosition().x,plane.getPosition().y);
-        sb.draw(tube.getTopTube(), tube.getPosTopTube().x , tube.getPosTopTube().y);
-        sb.draw(tube.getBottomTube(), tube.getPosBotTube().x , tube.getPosBotTube().y);
+        for(Tube tube : tubes){
+            sb.draw(tube.getTopTube(), tube.getPosTopTube().x,tube.getPosTopTube().y);
+            sb.draw(tube.getBottomTube(),tube.getPosBotTube().x,tube.getPosBotTube().y);
+        }
         sb.end();
     }
 
