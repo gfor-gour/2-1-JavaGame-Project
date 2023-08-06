326c4378996a90e1bfdc69e73ded1767d03bcca0
shahariat39 <mdshahariathossain@gmail.com>
2023-08-06 13:51:25 +0600

    6th commit

--- core/src/com/mygdx/game/states/PlayState.java
+++ core/src/com/mygdx/game/states/PlayState.java
@@ -5,15 +5,18 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.mygdx.game.Game;
 import com.mygdx.game.Sprites.Plane;
+import com.mygdx.game.Sprites.Tube;
 
 public class PlayState extends State  {
     private Plane plane;
     private Texture bg;
+    private Tube tube;
     public PlayState(GameStateManager gsm) {
         super(gsm);
         plane = new Plane(50,300);
         cam.setToOrtho(false, Game.WIDTH/2,Game.HEIGHT/2);
         bg = new Texture("bg.png");
+        tube = new Tube(100);
     }
 
     @Override
@@ -34,6 +37,8 @@ public class PlayState extends State  {
         sb.begin();
         sb.draw(bg, cam.position.x - (cam.viewportWidth/2),0);
         sb.draw(plane.getPlane(),plane.getPosition().x,plane.getPosition().y);
+        sb.draw(tube.getTopTube(), tube.getPosTopTube().x , tube.getPosTopTube().y);
+        sb.draw(tube.getBottomTube(), tube.getPosBotTube().x , tube.getPosBotTube().y);
         sb.end();
     }
 
